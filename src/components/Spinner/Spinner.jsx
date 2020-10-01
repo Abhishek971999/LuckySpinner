a3f313ad98941366155eb8c93938911bd3fdd4fe
Abhishek Mogaveera <37222497+Abhishek971999@users.noreply.github.com>
2020-10-01 12:49:11 +0530

    Updated images and styles

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
-import money from '../../Vector.png'
-import './Spinner.css'
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import {SPINNER_SPREADSHEET_ID,SPINNER_SHEET_ID,SPINNER_CLIENT_EMAIL,SPINNER_PRIVATE_KEY} from './spinner_credentials'
+import './Spinner.css';
+import money from '../../images/money.png'
+
+const SPREADSHEET_ID = SPINNER_SPREADSHEET_ID;
+const SHEET_ID = SPINNER_SHEET_ID;
+const CLIENT_EMAIL = SPINNER_CLIENT_EMAIL;
+const PRIVATE_KEY = SPINNER_PRIVATE_KEY;
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+
 export class Spinner extends Component {
     state={
         spinnerState:"circle"
       }
+      //Function to Rotate and Stop the spinner
       RotateSpinner = ()=>{
         this.setState({spinnerState:"circle start-rotating"});
         setTimeout(()=>{
           this.setState({spinnerState:"circle start-rotating stop-rotating"});
           const res = this.checkValue()
-        //   this.appendSpreadsheet({ web_client: "pwa", timestamp: "2020-10-14T18:40:16.092Z",spin_result_index:res })
+          let device = "web-pwa"
+          if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            device = "mobile-pwa"
+           }
+          this.appendSpreadsheet({ web_client: device, timestamp: new Date(),spin_result_index:res })
         },Math.floor(Math.random()*10000+1))
       }
+
+      //Function to Check Value overlapping the pin
       checkValue = ()=>{
         const elements = document.querySelectorAll(".circle-section")
         let max = 0;
@@ -25,6 +41,22 @@ export class Spinner extends Component {
         });
         return element
       }
+
+      //Function to append data to google spreadsheet
+       appendSpreadsheet = async (row) => {
+        try {
+          await doc.useServiceAccountAuth({
+            client_email: CLIENT_EMAIL,
+            private_key: PRIVATE_KEY,
+          });
+          await doc.loadInfo();
+          const sheet = doc.sheetsById[SHEET_ID];
+          const result = await sheet.addRow(row);
+        } catch (e) {
+          console.error('Error: ', e);
+        }
+      };
+
     render() {
         return (
             <>
